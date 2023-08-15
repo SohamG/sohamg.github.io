f943df18289b36926e88f6231e0a113fb2834d95
Soham S Gumaste <sgumas2@uic.edu>
2023-08-15 17:24:28 -0500

    Remove sus content.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -xe
 TZ="America/Chicago"
-emacs -Q --script build-site.el
+emacs -Q --script ./build-site.el
 # cp content/style.xsl public/ && echo "Copied style.xsl!"
 # cp content/logo.svg public/ && echo "Copied logo!"
 # cp content/favicon.png public/ && echo "Copied Favicon!"
-rsync -avzh content/static public
+rsync -avzh ./content/static public
