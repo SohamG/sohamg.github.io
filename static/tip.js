fba50338f15f67c3f019847791ea207413241291
gh-runner <gh@example.com>
2023-08-18 14:45:06 +0000

    Deploying to gh-pages from @ SohamG/sohamg.github.io@392e9f8d86243e3706f3bdae58cdda03f4017e9d 🚀

---
+++ static/tip.js
@@ -0,0 +1,32 @@
+let notipBtn = document.querySelector('#notipbtn');
+let notipMdl = document.querySelector('#notipmdl');
+notipBtn.addEventListener('click', () => {
+  notipMdl.showModal();
+});
+
+let customBtn = document.querySelector('#custombtn');
+let customMdl = document.querySelector('#custommdl');
+
+customBtn.addEventListener('click', () => {
+  customMdl.showModal();
+});
+
+let fifteenBtn = document.querySelector('#fifteenbtn');
+let fifteenMdl = document.querySelector('#fifteenmdl');
+
+fifteenBtn.addEventListener('click', () => {
+  fifteenMdl.showModal();
+});
+
+let twentyfiveBtn = document.querySelector('#twentyfivebtn');
+let twentyfiveMdl = document.querySelector('#twentyfivemdl');
+
+twentyfiveBtn.addEventListener('click', () => {
+  twentyfiveMdl.showModal();
+});
+let thirtyfiveBtn = document.querySelector('#thirtyfivebtn');
+let thirtyfiveMdl = document.querySelector('#thirtyfivemdl');
+
+thirtyfiveBtn.addEventListener('click', () => {
+  thirtyfiveMdl.showModal();
+});
